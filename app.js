be3e031fd8da5396d101a4dceb524f5124f73065
khurshid <95979088+khurshid28@users.noreply.github.com>
2022-02-24 21:41:52 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// app.use(express.json())
+app.use(express.json())
 app.get('/', function (req, res) {
   res.send({
     data:"hi,khurshid",
